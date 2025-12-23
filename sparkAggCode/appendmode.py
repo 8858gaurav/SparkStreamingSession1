4d9fee13c4ca4a8eec672e252bf13aeb8873896b
Gaurav Mishra <52245439+8858gaurav@users.noreply.github.com>
2025-12-23 12:39:34 +0530

    Create Spark streaming application in append mode
    Implement a Spark streaming application with append mode for processing orders.

---
+++ sparkAggCode/appendmode.py
@@ -0,0 +1,51 @@
+from pyspark.sql.functions import *
+from pyspark.sql import SparkSession
+
+
+if __name__ == '__main__':
+    print("creating spark session")
+
+    spark = SparkSession.builder \
+            .appName("streaming application") \
+            .config("spark.driver.bindAddress", 'localhost') \
+            .config("spark.ui.port", "4050") \
+            .config("spark.ui.port", "4050") \
+            .config("spark.sql.shuffle.partitions", 3) \
+            .master("local[2]") \
+            .getOrCreate()
+            # since the group by will create a 200 shuffle partitons.
+            # to reduce the time, it's better to decrease the no of partitons, when 
+            # we have a large no of micro batches with smalll-2 sizes. 
+
+            # Now adding the file 1 by 1 from temp folder to input dir
+    
+# 1. read the data
+    order_schema = 'order_id long, order_date date, order_customer_id long, order_status string'
+    orders_df = spark \
+            .readStream \
+            .format("json") \
+            .schema(order_schema) \
+            .option("path", "/Users/gauravmishra/Desktop/SparkStreamingSession1/UnboundedStreaming/inputdir") \
+            .load()
+    
+
+# 2. prcoessing logic
+    orders_df.createOrReplaceTempView("orders")
+    agg_orders = spark.sql("select order_status, count(*) as total from orders group by 1")
+
+# 3. write the sink
+# in this once we placed the file1.json, and then file2.json, you will see, it will accumulated from the previouc batch output
+# do the same thing with the append mode, update mode
+# delete the check point directory before running this script
+    query = agg_orders \
+            .writeStream \
+            .format("console") \
+            .outputMode("append") \
+            .option("checkpointLocation", "checkpointdir1") \
+            .start()
+
+    query.awaitTermination()
+
+# pyspark.errors.exceptions.captured.AnalysisException: [STREAMING_OUTPUT_MODE.UNSUPPORTED_OPERATION] 
+# Invalid streaming output mode: append. This output mode is not supported for streaming aggregations 
+# without watermark on streaming DataFrames/DataSets. SQLSTATE: 42KDE
